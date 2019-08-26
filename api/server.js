b2f2bcb53fe9ccebb4f83e24cded07d430125cbe
Jonathan Taylor <jonyonson@icloud.com>
2019-08-26 16:41:25 -0500

    update users resource

--- api/server.js
+++ api/server.js
@@ -2,17 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-// const authRouter = require('../users/auth-router.js');
-// const usersRouter = require('../users/users-router.js');
+const authRouter = require('../resources/users/auth-router.js');
+const usersRouter = require('../resources/users/users-router.js');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-
-// server.use('/auth', authRouter);
-// server.use('/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("IT'S ALIVE!!!");
