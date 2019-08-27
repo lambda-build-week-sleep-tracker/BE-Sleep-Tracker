add34283fea89607e2c9f3f1f1b26ddb29045dcb
Jonathan Taylor <jonyonson@icloud.com>
2019-08-27 17:31:42 -0500

    restructure

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const authRouter = require('../resources/users/auth-router.js');
-const usersRouter = require('../resources/users/users-router.js');
+const authRouter = require('./resources/users/auth-router.js');
+const usersRouter = require('./resources/users/users-router.js');
 
 const server = express();
 
